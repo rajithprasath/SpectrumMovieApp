0c927a5876de7af1933d559171f6429f3338c153
rajith.p.aet@gmail.com <rajith.p.aet@gmail.com>
2022-03-24 23:29:48 +0530

    UI improvements -> Imageview loading progress added

--- app/src/main/java/com/rajith/spectrummovieapp/view/adapters/MovieAdapter.kt
+++ app/src/main/java/com/rajith/spectrummovieapp/view/adapters/MovieAdapter.kt
@@ -1,5 +1,6 @@
 package com.rajith.spectrummovieapp.view.adapters
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +8,10 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.rajith.spectrummovieapp.R
 import com.rajith.spectrummovieapp.core.util.Constants.Companion.IMAGE_BASE_URL
 import com.rajith.spectrummovieapp.domain.model.Movie
@@ -29,7 +34,19 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = differ.currentList[position]
         holder.itemView.apply {
-            Glide.with(this).load(IMAGE_BASE_URL + movie.poster_path).into(ivPoster)
+            Glide.with(this)
+                .load(IMAGE_BASE_URL + movie.poster_path)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(p0: GlideException?, p1: Any?, p2: Target<Drawable>?, p3: Boolean): Boolean {
+                        imageProgressBar.visibility = View.GONE;
+                        return false
+                    }
+                    override fun onResourceReady(p0: Drawable?, p1: Any?, p2: Target<Drawable>?, p3: DataSource?, p4: Boolean): Boolean {
+                        imageProgressBar.visibility = View.GONE
+                        return false
+                    }
+                })
+                .into(ivPoster)
             tvTitle.text = movie.title
             tvReleaseDate.text = movie.release_date
             tvVoteCount.text = movie.vote_count.toString()
